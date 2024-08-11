bc287371fb32f761c9d7948e0c7643e5462e26e2
Cukehater <cukehater@gmail.com>
2024-08-11 17:07:45 +0900

    fix: 게시글 작성 날짜

--- app/shared/utils/dateFormat.ts
+++ app/shared/utils/dateFormat.ts
@@ -1,17 +1,26 @@
-import { differenceInMonths, parseISO, format as format_fns } from 'date-fns'
+import { differenceInMonths, format as format_fns } from 'date-fns'
 import { format, register } from 'timeago.js'
 import koLocale from 'timeago.js/lib/lang/ko'
 
 register('ko', koLocale)
 
 export function dateFormat(date: string) {
-  const parsedDate = parseISO(date)
-  const now = new Date()
-  const monthsDifference = differenceInMonths(now, parsedDate)
+  const timestamp = convertToTimestamp(date)
+
+  const currentDate = new Date()
+  const inputDate = new Date(timestamp)
+  const monthsDifference = differenceInMonths(currentDate, inputDate)
 
   if (monthsDifference >= 1) {
-    return format_fns(parsedDate, 'yyyy년 M월 d일')
+    return format_fns(inputDate, 'yyyy-MM-dd')
   } else {
-    return format(date, 'ko')
+    return format(timestamp, 'ko')
   }
+}
+
+const convertToTimestamp = (dateString: string): number => {
+  const [year, month, day, hour, minute, second] = dateString
+    .split('-')
+    .map(Number)
+  return new Date(year, month - 1, day, hour, minute, second).getTime()
 }
