9c1d2d7b7e1562b0979e2e9a00618b322d6ab1eb
Cukehater <cukehater@gmail.com>
2024-09-17 00:21:46 +0900

    feat: Route Protection

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,27 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export default async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET as string
-  })
-
   const response = NextResponse.next()
   if (req.nextUrl.pathname === '/write' || req.nextUrl.pathname === '/') {
     const searchParams = req.nextUrl.searchParams.toString()
     response.headers.set('x-search-params', searchParams)
   }
+
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET as string,
+    salt: ''
+  })
+
+  if (
+    req.nextUrl.pathname === '/write' ||
+    req.nextUrl.pathname === '/setting' ||
+    req.nextUrl.pathname === '/draft'
+  ) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+  }
+
   return response
 }
